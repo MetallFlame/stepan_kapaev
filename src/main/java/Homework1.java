8093716d75a57a855aa176a8389302ecafc95f7f
Artiom Kriskovets <artem1491@mail.ru>
2023-10-20 10:49:18 +0300

    решил задачу 1

--- src/main/java/Homework1.java
+++ src/main/java/Homework1.java
@@ -1,5 +1,5 @@
 public class Homework1 {
     public static void main(String[] args) {
-        
+
     }
 }
---
+++ src/main/java/lesson2.java
@@ -0,0 +1,13 @@
+public class lesson2 {
+
+        public static void main(String[] args) {
+           int sum = 1;
+           int count = 10;
+
+           for(int i = 1; i <= count; i++) {
+          sum = sum * i;;
+           }
+            System.out.println(sum);
+        }
+    }
+
