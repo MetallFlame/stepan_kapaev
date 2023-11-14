80cd41dec2b8a8a26cac9099860b67bb093ff2f8
Professional <stepa.kapaev@gmail.com>
2023-11-14 16:44:33 +0300

    ПОЧТИ СДЕЛАЛ

--- src/main/java/lesson2.java
+++ src/main/java/lesson2.java
@@ -1,13 +1,16 @@
 public class lesson2 {
 
         public static void main(String[] args) {
-           int sum = 1;
-           int count = 10;
+           int[] arr = new int[]{1, 2, 5, 6,};
+           for(int i = 0; i < 2; i++){
+               int x = arr[i];
+                arr[i] = arr[arr.length - i - 1];
+                arr[arr.length - i - 1] = x;
 
-           for(int i = 1; i <= count; i++) {
-          sum = sum * i;;
            }
-            System.out.println(sum);
+            for(int i = 0; i < arr.length; i++){
+                System.out.println(arr[i]);
+            }
         }
     }
 
