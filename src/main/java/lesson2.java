ebb968304ee75ea3c82adba9d1f567a3ec12d6b6
Artiom Kriskovets <artem1491@mail.ru>
2023-11-17 08:59:09 +0300

    исправил задачу 1

--- src/main/java/lesson2.java
+++ src/main/java/lesson2.java
@@ -1,16 +1,22 @@
 public class lesson2 {
 
-        public static void main(String[] args) {
-           int[] arr = new int[]{1, 2, 5, 6,};
-           for(int i = 0; i < 2; i++){
-               int x = arr[i];
-                arr[i] = arr[arr.length - i - 1];
-                arr[arr.length - i - 1] = x;
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 5, 6, 8, 5, 2};
+        sort(arr);
+        print(arr);
+    }
 
-           }
-            for(int i = 0; i < arr.length; i++){
-                System.out.println(arr[i]);
-            }
+    private static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
         }
     }
 
+    private static void sort(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            int x = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = x;
+        }
+    }
+}
