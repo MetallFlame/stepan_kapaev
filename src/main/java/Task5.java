0bcb337ce44e23e1b832d17f6ddc289571d332f9
Professional <stepa.kapaev@gmail.com>
2023-11-28 21:18:02 +0300

    Исправил ошибку в первой задаче

---
+++ src/main/java/Task5.java
@@ -0,0 +1,11 @@
+public class Task5 {
+    public static void main(String[] args) {
+        int radius = 10;
+
+        perimetr(radius);
+    }
+
+    private static void perimetr(int radius) {
+
+    }
+}
---
+++ src/main/java/Task6.java
@@ -0,0 +1,17 @@
+public class Task6 {
+    public static void main(String[] args) {
+        int w = 10;
+        int h = 17;
+
+        square(w, h);
+        perimetres(w, h);
+    }
+
+    private static void perimetres(int w, int h) {
+        System.out.println("Периметр треугольника равен " + w * h);
+    }
+
+    private static void square(int w, int h) {
+        System.out.println("Периметр прямоугольника равен " + (w + h) * 2);
+    }
+}
