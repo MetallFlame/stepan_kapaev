42eb70c6e6b929bb84d99e63fd7e7fcfa397473b
Professional <stepa.kapaev@gmail.com>
2023-11-29 08:30:12 +0300

    РЕШИЛ ЗАДАЧУ 10

---
+++ src/main/java/Task10.java
@@ -0,0 +1,22 @@
+public class Task10 {
+    public static void main(String[] args) {
+        int height = 175;
+        int weight = 80;
+
+        idealWeight(height, weight);
+    }
+
+    private static void idealWeight(int height, int weight) {
+        int idealWeight = height - 110;
+        if (weight == idealWeight){
+            System.out.println("Ваш вес идеальный");
+            return;
+        }
+        if (idealWeight > weight){
+            System.out.println("Ваш идеальный вес - " + idealWeight + " кг" + ".Вам нужно набрать " + (idealWeight - weight) + " кг");
+        }
+        else{
+            System.out.println("Ваш идеальный вес - " + idealWeight + " кг" + ".Вам нужно сбросить " + (weight - idealWeight) + " кг");
+        }
+    }
+}
