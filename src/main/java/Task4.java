d1ef09205195cd73120c4e6f01f0737134ad67af
Professional <stepa.kapaev@gmail.com>
2023-11-29 09:52:07 +0300

    РЕШИЛ ЗАДАЧУ 4

---
+++ src/main/java/Task4.java
@@ -0,0 +1,12 @@
+
+
+public class Task4 {
+    public static void main(String[] args) {
+        hiWdPrint();
+    }
+
+    private static void hiWdPrint() {
+        System.out.println("Hello");
+        System.out.println("world");
+    }
+}
