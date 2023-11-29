107b487e5caea7a889e513fef6e319ccd38be788
Professional <stepa.kapaev@gmail.com>
2023-11-29 10:28:10 +0300

    Возникли проблемы Оно не распознаёт perimetr, square

--- src/main/java/Task5.java
+++ src/main/java/Task5.java
@@ -1,11 +1,24 @@
 public class Task5 {
     public static void main(String[] args) {
-        int radius = 10;
+        int radius = 15;
 
         perimetr(radius);
+        System.out.println(perimetr);
+        square(radius);
+        System.out.println(square);
     }
 
-    private static void perimetr(int radius) {
+    private static float square(int radius) {
+        float pCount = 3.14F;
 
+        float square = radius * radius * pCount;
+        return (square);
+    }
+
+    private static float perimetr(int radius) {
+        float pCount = 3.14F;
+
+        float perimetr = radius * 2 * pCount;
+        return (perimetr);
     }
 }
