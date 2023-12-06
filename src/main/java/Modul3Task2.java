9aba78d8302bc9aadc84071b5385bd28689c00f2
Professional <stepa.kapaev@gmail.com>
2023-12-06 11:07:47 +0300

    Решил задачу 2 и 3

--- src/main/java/Modul3Task2.java
+++ src/main/java/Modul3Task2.java
@@ -10,11 +10,9 @@ public class Modul3Task2 {
     private static void hello(int oClock) {
         if ((oClock >= 7) && (oClock <= 12)) {
             System.out.println("Доброе утро!");
-            return;
         }
         if ((oClock >= 13) && (oClock <= 18)) {
             System.out.println("Добрый день!");
-            return;
         }
         if ((oClock >= 19) && (oClock <=24)) {
             System.out.println("Добрый вечер!");
