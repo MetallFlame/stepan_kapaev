97898d56ff95c038655de43a5af14bff995a6f86
Professional <stepa.kapaev@gmail.com>
2023-12-18 10:44:28 +0300

    Решил задачу 9

--- src/main/java/module03/Task9.java
+++ src/main/java/module03/Task9.java
@@ -3,20 +3,23 @@ package module03;
 public class Task9 {
     public static void main(String[] args) {
         int xCoordinates1 = 3;
-        int yCoordinates1 = 3;
+        int yCoordinates1 = 10;
         int xCoordinates2 = 15;
-        int yCoordinates2 = 3;
+        int yCoordinates2 = 30;
 
         movingTraectory(xCoordinates1, xCoordinates2, yCoordinates1, yCoordinates2);
     }
 
     private static void movingTraectory(int xCoordinates1, int xCoordinates2, int yCoordinates1, int yCoordinates2) {
-        boolean upDown = false;
+        boolean upDown;
         boolean horVer;
         boolean usual = false;
         boolean ground = false;
         if (yCoordinates1 == yCoordinates2){
             ground = true;
+        }
+        if (yCoordinates1 > yCoordinates2){
+            upDown = false;
         }
         else {
             upDown = true;
@@ -39,10 +42,10 @@ public class Task9 {
         if ((upDown == true)&&(usual == true)){
             System.out.println("подъём");
         }
-        if ((upDown == true)&&(horVer == false)){
+        if ((upDown == true)&&(horVer == false)&&(usual == false)){
             System.out.println("Отвесный подъём");
         }
-        if ((upDown == false)&&(horVer == false)){
+        if ((upDown == false)&&(horVer == false)&&(usual == false)&&(ground == false)){
             System.out.println("Отвесный спуск");
         }
     }
