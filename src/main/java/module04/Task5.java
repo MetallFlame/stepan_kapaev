52a37a12a38f03d9aef9f3a9758d09fc6b3d2d84
Professional <stepa.kapaev@gmail.com>
2023-12-22 21:56:16 +0300

    Решил задачу 9

--- src/main/java/module04/Task5.java
+++ src/main/java/module04/Task5.java
@@ -2,16 +2,20 @@ package module04;
 
 public class Task5 {
     public static void main(String[] args) {
-        int number = 12344391;
+        int number = 535;
 
-        String num = Integer.toString(number);
-        String num1 = num.                                       ;
-        if (num == num1){
-            System.out.println("YES");
-        }
-        else {
-            System.out.println("NO");
-        }
-
+        isReversive(number);
     }
+
+    private static void isReversive(int number) {
+        String number1 = new String(Integer.toString(number));
+        StringBuilder name = new StringBuilder(Integer.toString(number));
+        name.reverse();
+        String name1 = name.toString();
+        if (name1 == number1){
+            System.out.println("it's true");
+        }
+    }
+
 }
+
