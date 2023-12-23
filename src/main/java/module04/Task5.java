f3d6adcd51315079371d19fa12206deea72d9d37
Professional <stepa.kapaev@gmail.com>
2023-12-23 18:39:23 +0300

    Решил задачу 5

--- src/main/java/module04/Task5.java
+++ src/main/java/module04/Task5.java
@@ -2,7 +2,7 @@ package module04;
 
 public class Task5 {
     public static void main(String[] args) {
-        int number = 535;
+        int number = 73693;
 
         isReversive(number);
     }
@@ -12,8 +12,11 @@ public class Task5 {
         StringBuilder name = new StringBuilder(Integer.toString(number));
         name.reverse();
         String name1 = name.toString();
-        if (name1 == number1){
+        if (name1.equals(number1)){
             System.out.println("it's true");
+        }
+        else {
+            System.out.println("It's false");
         }
     }
 
