ffc75856eba52d808d559c3f34b2928a085b336c
Professional <stepa.kapaev@gmail.com>
2024-01-11 12:13:50 +0300

    Решил задачу 3

---
+++ src/main/java/module04/module2Task3.java
@@ -0,0 +1,48 @@
+package module04;
+
+
+public class module2Task3 {
+    public static void main(String[] args) {
+        int number = 25;
+        boolean emptyFull = true;
+
+        isoscelesTriagle(number, emptyFull);
+    }
+
+    private static void isoscelesTriagle(int number, boolean emptyFull) {
+        star(number);
+        body(number, emptyFull);
+        line(number);
+    }
+
+    private static void body(int number, boolean emptyFull) {
+        for (int i = 2; i <= number; i++) {
+            for (int j = 1; j < number - i + 2; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            for (int j = 0; j < (1 + 2 * (i - 2)); j++){
+                if (emptyFull){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("*");
+        }
+    }
+
+    private static void line(int number) {
+        for (int i = 0; i <= number * 2; i++){
+            System.out.print("*");
+        }
+    }
+
+    private static void star(int number) {
+        for (int i = 1; i <= number; i++){
+            System.out.print(" ");
+        }
+        System.out.println("*");
+    }
+}
