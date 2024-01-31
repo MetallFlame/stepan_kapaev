75db78d6929798e9256099b4464aefb0ad839f3c
Professional <stepa.kapaev@gmail.com>
2024-01-31 16:05:04 +0300

    Решаю задачу 12

--- src/main/java/module5/Task12.java
+++ src/main/java/module5/Task12.java
@@ -2,7 +2,7 @@ package module5;
 
 public class Task12 {
     public static void main(String[] args) {
-        String name = ":---))";
+        String name = ":----))";
 
         boolean real = isSmile(name);
         System.out.println(real);
@@ -10,27 +10,10 @@ public class Task12 {
 
     private static boolean isSmile(String name) {
         boolean real = false;
-        if ((name.charAt(name.length() - 1) == '(')|(name.charAt(name.length() - 1) == ')')|(name.charAt(name.length() - 1) == '{')|(name.charAt(name.length() - 1) == '}')|(name.charAt(name.length() - 1) == '[')|(name.charAt(name.length() - 1) == ']')) {
-            if (name.charAt(name.length() - 1) == name.charAt(name.length() - 2)) {
-                if ((name.charAt(0) == ';') | (name.charAt(0) == ':')) {
-                    StringBuilder name1 = new StringBuilder(name);
-                    name1.delete(name1.length() - 2, name1.length() - 1);
-                    name1.deleteCharAt(0);
-                    if (name1.isEmpty()){
-                        real = true;
-                    }
-                    else {
-                        String name2 = name1.toString();
-                        char[] name3 = name2.toCharArray();
-                        for (int i = 0; i < name3.length; i++){
-                            if ((name3[i] == '-')|(name3[i] == '(')|(name3[i] == ')')|(name3[i] == '[')|(name3[i] == ']')|(name3[i] == '{')|(name3[i] == '}')){
-                                real = true;
-                            }
-                        }
-                    }
-                }
-            }
-        }
+
         return real;
     }
 }
+
+
+
