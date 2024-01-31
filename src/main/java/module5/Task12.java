88ba7c74a8a1138c9b3f935de9eddfc435ee8bf3
Professional <stepa.kapaev@gmail.com>
2024-01-31 17:11:30 +0300

    Решил задачу 12

--- src/main/java/module5/Task12.java
+++ src/main/java/module5/Task12.java
@@ -2,7 +2,7 @@ package module5;
 
 public class Task12 {
     public static void main(String[] args) {
-        String name = ":----))";
+        String name = ";-]";
 
         boolean real = isSmile(name);
         System.out.println(real);
@@ -10,7 +10,18 @@ public class Task12 {
 
     private static boolean isSmile(String name) {
         boolean real = false;
-
+        char[] name1 = name.toCharArray();
+        if ((name1[0] == ':')|(name1[0] == ';')){
+            if ((name1[name.length() - 1] == '(')|(name1[name.length() - 1] == ')')|(name1[name.length() - 1] == '[')|(name1[name.length() - 1] == ']')|(name1[name.length() - 1] == '{')|(name1[name.length() - 1] == '}')){
+                int sum = 0;
+                for (int i = 1; i < name1.length - 1; i++){
+                    if ((name1[i] == '(')|(name1[i] == ')')|(name1[i] == '[')|(name1[i] == ']')|(name1[i] == '{')|(name1[i] == '}')|(name1[i] == '-')){
+                       sum++;
+                    }
+                }
+                real = sum == name1.length - 2;
+            }
+        }
         return real;
     }
 }
