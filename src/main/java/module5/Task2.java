df7fb0cea7873157191a5d5c8f39aa7c2d7088cb
Professional <stepa.kapaev@gmail.com>
2024-02-02 15:14:42 +0300

    Исправил задачу 2

--- src/main/java/module5/Task2.java
+++ src/main/java/module5/Task2.java
@@ -2,17 +2,20 @@ package module5;
 
 public class Task2 {
     public static void main(String[] args) {
-        int number = 100;
+        int number = 0;
         int n = 10;
-        String name = counts(number);
+        char name = counts(number, n);
         System.out.println(name);
     }
 
-    private static String counts(int num) {
+    private static char counts(int num, int n) {
         StringBuilder name = new StringBuilder();
         for (int i = num; name.length() <= 1000; i++){
             name.append(i);
         }
-        return name.toString();
+        String name1 = name.toString();
+        char[] name2 = name1.toCharArray();
+        System.out.println(name);
+        return name2[n - 1];
     }
 }
